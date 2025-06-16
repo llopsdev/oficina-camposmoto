2eb315adb23101d56b76da8012a0200a66e48215
Luan Lopes <llopsdev@gmail.com>
2025-06-16 02:46:44 -0300

    commit inicial: projeto spring boot com estrutura basica

---
+++ src/main/java/br/com/motocampos/system/MotoCamposSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.motocampos.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoCamposSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoCamposSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/motocampos/system/MotoCamposSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.motocampos.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoCamposSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
