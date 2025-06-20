575faf4b8f1b84868ced4539ee973a840d25b3ab
Luan Lopes <llopsdev@gmail.com>
2025-06-20 03:35:07 -0300

    Implementação na CLienteController dos metodos: mostrarFormulario,editar, salvar, deletar Cliente

--- src/main/java/br/com/motocampos/system/controller/ClienteController.java
+++ src/main/java/br/com/motocampos/system/controller/ClienteController.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.motocampos.system.model.Cliente;
 import br.com.motocampos.system.service.ClienteService;
@@ -25,6 +28,29 @@ public class ClienteController {
 		List<Cliente> list = service.findAll();
 		model.addAttribute("clientes",list);
 		return "clientes/listar";
+	}
+	@GetMapping("/form")
+	public String mostrarFormulario(Model model) {
+		Cliente cliente = new Cliente();
+		model.addAttribute("cliente", cliente);
+		return "clientes/form";
+	}
+	
+	@PostMapping
+	public String salvarCliente(Cliente cliente) {
+		service.save(cliente);
+		return "redirect:/clientes";
+	}
+	@GetMapping("/editar/{id}")
+	public String mostrarFormularioEdicao(@PathVariable Integer id, Model model) {
+		Cliente cliente = service.buscarPorId(id).orElseThrow();
+		model.addAttribute("cliente", cliente);
+		return "clientes/form";
+	}
+	@PostMapping
+	public String deletarCliente(@RequestParam("id") Integer id) {
+		Cliente cliente= service.buscarPorId(id).orElseThrow(() -> new IllegalArgumentException("Cliente não encontrado"));
+		return "redirect/clientes";
 	}
 	
 	
